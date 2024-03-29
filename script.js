b4d8d5d2208178577ef2f276746a65fa1ead2ef1
devLucas0110 <joaolucas.aqalves111@outlook.com>
2024-03-28 22:07:20 -0300

    finished logic decrement minutes

--- script.js
+++ script.js
@@ -1,18 +1,52 @@
 let verifyInputMinutes = false;
-var min;
+let min;
 
-function start(){
+function decreaseMinutes() {
+
+    if (min > 0) {
+        min--;
+        updateParagraph();
+    }
+    
+}
+
+function start() {
     const minutes = document.querySelector('input').value;
+    
+    rmInput();
 
-    if(!verifyInputMinutes){
+    if (!verifyInputMinutes) {
         min = minutes;
         verifyInputMinutes = true;
+        addParagraph();
     }
 
-    function lessMinutes(){   
-        if(min > 0){
-            min--;
-        }
-        return min
+    setInterval(decreaseMinutes, 1000);
+
+}
+
+function rmInput() {
+    var removeInput = document.getElementById('input-minutes');
+    removeInput.parentElement.removeChild(removeInput);
+}
+
+function addParagraph() {
+    var container = document.getElementById("input");
+    var paragraph = document.createElement('h1');
+    paragraph.id = 'paragraph'
+    paragraph.innerHTML = min;
+    container.append(paragraph);
+}
+
+function updateParagraph() {
+    var paragraph = document.getElementById('paragraph');
+    if (paragraph) {
+        paragraph.innerHTML = min;
     }
 }
+
+function rmParagraph() {
+    var removeParagraph = document.getElementById('paragraph');
+    removeParagraph.parentElement.removeChild(removeParagraph);
+}
+
