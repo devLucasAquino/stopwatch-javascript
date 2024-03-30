cc0e9d9d262fc748cfb32399ca3b8e20434eb6ed
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-03-29 22:18:18 -0300

    feat: added logic function decreaseSeconds

--- script.js
+++ script.js
@@ -1,55 +1,63 @@
-let verifyInputMinutes = false;
+let verifyInput = false;
 let min;
-
-let verifyInputSeconds = false;
 let sec;
 
 function decreaseMinutes() {
 
     if (min > 0) {
         min--;
-        updateParagraph(min);
+        updateParagraph(min, 'paragraph-minutes');
+    }
+    
+}
+
+function decreaseSeconds() {
+
+    if (sec > 0) {
+        sec--;
+        updateParagraph(sec, 'paragraph-seconds');
     }
     
 }
 
 function start() {
-    const minutes = document.querySelector('input').value;
+    const minutes = document.getElementById('input-minutes').value;
+    const seconds = document.getElementById('input-seconds').value;
 
     rmInput();
 
-    if (!verifyInputMinutes) {
+    if (!verifyInput) {
         min = minutes;
+        sec = seconds;
         verifyInputMinutes = true;
-        addParagraph(min, 'container-minutes');
+        addParagraph(min, 'container-minutes', 'paragraph-minutes');
+        addParagraph(sec, 'container-seconds', 'paragraph-seconds');
     }
 
     setInterval(decreaseMinutes, 1000);
+    setInterval(decreaseSeconds, 1000);
 
 }
 
 function rmInput() {
-    var removeInput = document.getElementById('input-minutes');
-    removeInput.parentElement.removeChild(removeInput);
+    var removeInputMin = document.getElementById('input-minutes');
+    var removeInputSec = document.getElementById('input-seconds');
+    removeInputMin.parentElement.removeChild(removeInputMin);
+    removeInputSec.parentElement.removeChild(removeInputSec);
 }
 
-function addParagraph(time, id) {
+function addParagraph(time, id, id_element) {
     var container = document.getElementById(`${id}`);
     var paragraph = document.createElement('h1');
-    paragraph.id = 'paragraph'
+    paragraph.id = `${id_element}`
     paragraph.innerHTML = time;
     container.append(paragraph);
 }
 
-function updateParagraph(time) {
-    var paragraph = document.getElementById('paragraph');
+function updateParagraph(time, id_element) {
+    var paragraph = document.getElementById(`${id_element}`);
     if (paragraph) {
         paragraph.innerHTML = time;
     }
 }
-
-// function rmParagraph() {
-//     var removeParagraph = document.getElementById('paragraph');
-//     removeParagraph.parentElement.removeChild(removeParagraph);
-// }
 
